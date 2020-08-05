fb373c73b75cd9144c5f8d8ab8b8f2f41af3c2c4
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-08-05 16:12:17 +0000

    Improves docker build script

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -3,9 +3,14 @@
 #set -e
 #set -o pipefail
 
-# add user to docker group
-#sudo usermod -aG docker $USER
-#newgrp docker
+# check if the docker command was found (assumes that docker is installed)
+if ! command -v docker &> /dev/null
+then
+  echo "Run manually following commands prior executing this script to add the current user to the docker group:"
+  echo "  sudo usermod -aG docker $USER"
+  echo "  newgrp docker"
+  exit 1
+fi
 
 # build image
 TS=`date +%s`
